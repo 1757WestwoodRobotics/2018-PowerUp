b7fcc103280bf47b748db359af81377701b05af4
Chetan <cb_gopal@yahoo.com>
2018-02-10 15:57:01 -0500

    Started work on I2C calls to communicate with Arduino.

--- src/main/java/org/whsrobotics/communications/ArduinoI2C.java
+++ src/main/java/org/whsrobotics/communications/ArduinoI2C.java
@@ -1,4 +1,28 @@
 package org.whsrobotics.communications;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import org.whsrobotics.utils.RobotLogger;
+
+
 public class ArduinoI2C {
+    private I2C i2c;
+
+    public  ArduinoI2C(int deviceAddress) {
+        i2c = new I2C(Port.kOnboard, deviceAddress);
+    }
+
+    public boolean writeData(char[] charArray){
+
+
+        // convert char stream to byte stream
+        byte[] WriteData = new byte[charArray.length];
+        byte[] ReadData = new byte[0];
+
+        for (int i = 0; i < charArray.length; i++) {
+            WriteData[i] = (byte) charArray[i];
+        }
+        RobotLogger.log(this.getClass(), new String(charArray));
+        return i2c.transaction(WriteData, WriteData.length, ReadData, 0);
+    }
 }
