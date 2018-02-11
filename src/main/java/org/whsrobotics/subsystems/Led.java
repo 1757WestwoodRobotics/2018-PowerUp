f97d25907a5e0f87dd4b1b4edb48d1002346d232
Chetan Gopal <cb_gopal@yahoo.com>
2018-02-11 10:08:24 -0500

    LED Utility call to control LED behavior. Still needs to be integrated and tested.

--- src/main/java/org/whsrobotics/subsystems/Led.java
+++ src/main/java/org/whsrobotics/subsystems/Led.java
@@ -1,4 +1,39 @@
 package org.whsrobotics.subsystems;
 
+import org.whsrobotics.communications.ArduinoI2C;
+import org.whsrobotics.utils.RobotLogger;
+
+/*
+    LED Utility class to control light from the robot.
+ */
+
+
 public class Led {
+    // this is the address we will use to communicate on the i2c wire between Rio and Arduino
+    private static final int ledAddress = 4;
+    private static Led instance;
+    private static ArduinoI2C arduino;
+
+    /*
+    We have to define command protocol between Rio and Arduino to do specific actions
+    with Strip Lights or Ring Lights; like changing colors or fireworks etc.
+    */
+    private Led() {
+
+        try {
+            if (arduino == null) {
+                arduino = new ArduinoI2C(ledAddress);
+            }
+
+        } catch (NullPointerException e) {
+            RobotLogger.err(this.getClass(), "Error instantiating the Led control!" + e.getMessage());
+        }
+    }
+    public static Led getInstance() {
+        if (instance == null) {
+            instance = new Led();
+        }
+
+        return instance;
+    }
 }
