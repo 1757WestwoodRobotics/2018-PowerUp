f91ebe830c3449cc6ca96846dede55ab43dcdfbe
Chetan <cb_gopal@yahoo.com>
2018-02-11 15:32:46 -0500

    Clean up and added a few methods. I2C still not working with Arduino.

--- src/main/java/org/whsrobotics/communications/ArduinoI2C.java
+++ src/main/java/org/whsrobotics/communications/ArduinoI2C.java
@@ -6,28 +6,49 @@ import org.whsrobotics.utils.RobotLogger;
 
 public class ArduinoI2C {
 
-    private I2C i2c;
+    private I2C wire;
+    private int address;
+    private static final int MAX_BYTES = 32;
 
     public ArduinoI2C(int deviceAddress) {
-        i2c = new I2C(Port.kOnboard, deviceAddress);
+        address = deviceAddress;
+        wire = new I2C(Port.kOnboard, address);
     }
 
     /*
     Here Roborio is the master and Arduino is the slave
-    If write is a sucess, return will be fale else true
+    If write is aborted, return will be false else true
     */
     public boolean writeData(char[] charArray){
 
         // convert char stream to byte stream
         byte[] WriteData = new byte[charArray.length];
-        byte[] ReadData = new byte[0];
+        boolean failed;
 
         for (int i = 0; i < charArray.length; i++) {
             WriteData[i] = (byte) charArray[i];
         }
 
-        RobotLogger.log(this.getClass(), new String(charArray));
+        RobotLogger.log(this.getClass(), "#### Writing message: " + new String(charArray));
 
-        return i2c.transaction(WriteData, WriteData.length, ReadData, 0);
+        failed = wire.writeBulk(WriteData, WriteData.length);
+        return failed;
+    }
+
+    public String readData() {
+
+        byte[] data = new byte[MAX_BYTES];//create a byte array to hold the incoming data
+        String returnData;
+
+        boolean failed;
+
+        failed = wire.read(address, MAX_BYTES, data);//use address 4 on i2c and store it in data
+
+        if (failed) {
+            return "";
+        }
+        else {
+            return new String(data);//create a string from the byte array
+        }
     }
 }
