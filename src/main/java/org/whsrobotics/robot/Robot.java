063f97cd5447ab94f32919d8d6d383bb9107c6d3
Chetan Gopal <cb_gopal@yahoo.com>
2018-02-11 10:17:09 -0500

    Instantiation of Led Subsystem in Robo Init.

--- src/main/java/org/whsrobotics/robot/Robot.java
+++ src/main/java/org/whsrobotics/robot/Robot.java
@@ -3,6 +3,7 @@ package org.whsrobotics.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import org.whsrobotics.subsystems.DriveTrain;
+import org.whsrobotics.subsystems.Led;
 import org.whsrobotics.subsystems.Vision;
 import org.whsrobotics.utils.RobotLogger;
 
@@ -14,6 +15,7 @@ public class Robot extends TimedRobot {
 
         OI.getInstance();
         DriveTrain.getInstance();
+        Led.getInstance();
     }
 
     // ------------ AUTONOMOUS METHODS ------------- //
