3d168146d438bccca8bbe51c9d982f613be71983
Michael Serratore <21mserratore@wpsstudents.org>
2018-02-21 17:39:46 -0500

    Add DriveForward

---
+++ src/main/java/org/whsrobotics/commands/DriveForwardTimed.java
@@ -0,0 +1,13 @@
+package org.whsrobotics.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveForwardTimed extends TimedCommand{
+
+    public DriveForwardTimed(double timeout) {
+        super(new DriveForward(), timeout);
+
+
+
+    }
+}
