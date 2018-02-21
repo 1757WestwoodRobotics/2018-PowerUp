fc5992e4e4191bb82c082b6b4b3ff8614560a20e
Larry Tseng <lt222888@gmail.com>
2018-02-21 17:27:10 -0500

    Add Autonomous template

---
+++ src/main/java/org/whsrobotics/robot/Autonomous.java
@@ -0,0 +1,46 @@
+package org.whsrobotics.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import org.whsrobotics.utils.RobotLogger;
+
+public class Autonomous {
+
+    // TODO: Convert to a singleton
+
+    private static String gameData = "";
+    private static DriverStation.Alliance alliance = DriverStation.Alliance.Invalid;
+
+    public enum FieldTarget {
+        CROSS_LINE, PC_ZONE, EXCHANGE,
+        LEFT_SWITCH, RIGHT_SWITCH, LEFT_SCALE, RIGHT_SCALE;
+    }
+
+    public enum StartingPosition {
+        LEFT, MIDDLE, RIGHT;
+    }
+
+    public static String getGameData() {
+        if (gameData.isEmpty()) {
+            try {
+                gameData = DriverStation.getInstance().getGameSpecificMessage();
+            } catch (Exception e) {
+                RobotLogger.err(Autonomous.class, "Error with getting the Game Data! " + e.getMessage());   // TODO: singleton
+            }
+        }
+
+        return gameData;
+    }
+
+    public static DriverStation.Alliance getAlliance() {
+        if (alliance == DriverStation.Alliance.Invalid) {
+            try {
+                alliance = DriverStation.getInstance().getAlliance();
+            } catch (Exception e) {
+                RobotLogger.err(Autonomous.class, "Error with getting the Alliance Data! " + e.getMessage());   // TODO: Singleton
+            }
+        }
+
+        return alliance;
+    }
+
+}
