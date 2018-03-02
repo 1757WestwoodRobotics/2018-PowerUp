802361a6a550b5c7203394052ad997feb855a2b7
Larry Tseng <lt222888@gmail.com>
2018-03-02 10:01:43 -0500

    Add some Vision NetworkTables code

--- src/main/java/org/whsrobotics/communications/JetsonLAN.java
+++ src/main/java/org/whsrobotics/communications/JetsonLAN.java
@@ -1,6 +1,9 @@
 package org.whsrobotics.communications;
 
 import edu.wpi.first.networktables.*;
+import org.whsrobotics.robot.Robot;
+import org.whsrobotics.subsystems.Vision;
+import org.whsrobotics.utils.RobotLogger;
 
 public class JetsonLAN {
 
@@ -11,9 +14,7 @@ public class JetsonLAN {
         instance = NetworkTableInstance.getDefault();
         table = instance.getTable("Jetson");
 
-        table.addEntryListener("Jcommand", (table, key, entry, value, flags) -> {
-            // Do something
-        }, EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+        addNTListeners();
 
     }
 
@@ -25,10 +26,23 @@ public class JetsonLAN {
         table.getEntry(key).setValue(data);
     }
 
-    public static void waitForDataChange() {
-
-    }
+//    public static void waitForDataChange() {
+//
+//    }
 
     // https://wpilib.screenstepslive.com/s/currentCS/m/75361/l/843364-listening-for-value-changes
+
+    private static void addNTListeners() {
+        table.addEntryListener("Jcommand",
+                (table, key, entry, value, flags) -> Vision.receiveJcommand((int) value.getDouble()),
+                EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+        // TODO: Implement the other commands, set to Vision variables
+    }
+
+    private static void sendRcommand(Vision.Rcommand rcommand) {
+        writeData("Rcommand", rcommand.value);
+        RobotLogger.getInstance().log(instance.getClass(), "##### Sending Jetson Rcommand: " + rcommand.toString());
+    }
 
 }
